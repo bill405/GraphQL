3a37628f624708b414ee158d73d0fc660dcce278
Bill Henry <bill.henry405@gmail.com>
2018-10-05 13:37:30 -0600

    Add sample files to learn import/export

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,11 @@
-console.log('hello bil');
+//default and named exports
+import myCurrentLocation, {message, message2, getGreeting} from './myModule';
+import adder, {mySubtractor} from './math';
+
+console.log(message);
+console.log(message2);
+console.log(myCurrentLocation);
+console.log(getGreeting('Billy'));
+
+console.log(adder(1,2));
+console.log(mySubtractor(5, 1));
---
+++ graphql-basics/src/math.js
@@ -0,0 +1,9 @@
+
+
+let myAdder = (a,b) => a + b;
+let mySubtractor = (a,b) => a - b;
+
+export {
+    myAdder as default,
+    mySubtractor
+}
