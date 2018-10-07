410b441d281b8cd7df8f15fefc044e26bf019fe4
Bill Henry <bill.henry405@gmail.com>
2018-10-06 18:33:18 -0600

    added new test file and mutation of createUser

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,5 @@
 import {GraphQLServer} from 'graphql-yoga';
+import uuidv4 from 'uuid/v4';
 
 import {users, posts, comments} from './../seed/seed-data';
 
@@ -9,6 +10,10 @@ const typeDefs = `
         comments: [Comment!]!
         me: User!
         post: Post!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
     }
 
     type User {
@@ -79,6 +84,26 @@ const resolvers = {
                published: true
            }
        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some(user => user.email === args.email)
+            
+            if (emailTaken) {
+                throw new Error('Email was taken!')
+            }
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            }
+
+            users.push(user)
+
+            return user
+        }
     },
     Post: {
         author(parent, args, ctx, info) {
---
+++ test.js
@@ -0,0 +1,5 @@
+//const uuidv4 = require('uuid/v4')
+import v4 from 'uuid/v4';
+const id = v4();
+
+console.log(id)
